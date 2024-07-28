230df2fdd6178b0ad264764bbad5f685adaae31a
George <rozov2004@list.ru>
2024-07-28 16:12:20 +0300

    wezterm background image

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,9 +2,20 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.window_background_opacity = 1.0
+config.background = {
+    {
+        source = {
+            File = "/home/snuff/wallpapers/island.jpg"
+        },
+        hsb = {
+            brightness = 0.2,
+            hue = 1.0,
+            saturation = 1.2,
+        },
+    }
+}
 
 config.color_scheme = 'rose-pine'
-config.font = wezterm.font ('JetBrains Mono', {weight = 'Bold', italic = false})
+config.font = wezterm.font('JetBrains Mono', { weight = 'Bold', italic = false })
 
 return config
