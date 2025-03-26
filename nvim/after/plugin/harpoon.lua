ca81aa0a830f5d08ca9452225617d77f307168a0
George <rozov2004@list.ru>
2025-03-26 17:12:57 +0300

    feat: harpoon cmd list

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,7 +1,15 @@
 local harpoon = require("harpoon")
+local extensions = require("harpoon.extensions")
 
 -- REQUIRED
-harpoon:setup()
+harpoon:setup({
+    --cmd list for project-specific commands execution
+    cmd = {
+        select = function(list_item, list, option)
+            vim.cmd(list_item.value)
+        end
+    }
+})
 -- REQUIRED
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
@@ -15,3 +23,8 @@ vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+
+-- cmd key bindings
+vim.keymap.set("n", "<C-[>", function() harpoon.ui:toggle_quick_menu(harpoon:list("cmd")) end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list("cmd"):select(1) end)
+vim.keymap.set("n", "<C-m>", function() harpoon:list("cmd"):select(2) end)
